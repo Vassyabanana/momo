71e28e67a1359381b9c05138709115d511db9603
matteomnr <matteo.mnr@eduge.ch>
2018-05-16 12:52:41 +0200

    première intégration

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,85 +1,38 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="hello corps">
+    <div class="navbar">
+      <a href="Site.vue">Home</a>
+      <a href="#Introduction">Introduction</a>
+
+      <div class="dropdown">
+        <button class="dropbtn" onclick="myFunction()">Chronologie
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content" id="myDropdown">
+          <a href="Enfance.vue">Enfance</a>
+          <a href="Debuts.vue">Débuts</a>
+          <a href="#">Premières Inventions</a>
+        </div>
+      </div>
+      <a href="Gallerie.vue">Gallerie</a>
+      <a href="#chronologie">Vidéos</a>
+      <a href="#chronologie">Sources</a>
+
+    </div>
+
+  <h3>Débuts</h3>
+
+  <img src="https://i.pinimg.com/originals/49/07/49/4907496a53438cbcd8dc17aa86bb376f.jpg" alt="Magasin" style=" width:460px ">
+
+  <div class="inner-border">
+      <p class="article">
+      <br/>
+
+      En 1888, ils construisirent une presse avec laquelle ils purent publier leur journal : le West Side News. En 1892, ils ouvrirent un magasin de bicyclettes et se mirent quelques années plus tard à en construire eux-même.
+  <br/>
+  Une légende veut que ces modestes fabricants de vélos, s'ennuyant dans leur atelier, aient inventé l'avion par hasard, sans aide extérieure. En réalité, les frères sont des ingénieurs doués qui se sont inspirés de pionniers de l'aviation tels qu'Otto Lilienthal ou Octave Chanute pour concevoir le Flyer après cinq années d'expérimentations (cerfs-volants, planeurs, soufflerie), de recherche et de vols d'essais<br/><br/><br/>
+  </p>
+  </div>
   </div>
 </template>
 
@@ -110,4 +63,314 @@ li {
 a {
   color: #42b983;
 }
+/*
+.corps {
+    background-image: url( "https://78.media.tumblr.com/6e9049e02a6a9eb0d87fdf1de96a5366/tumblr_inline_o67yh3sJiw1sa90vo_540.jpg");
+}
+
+h1 {
+    color: #333300;
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+    font-size: 40px;
+    border-style: none;
+    background-color: #ffffff;
+
+}
+
+p.article {
+    color: #e6fff9;
+    font-family: Arial ;
+    text-align: justify;
+    font-size: 16px;
+    letter-spacing: 0.5px;
+    line-height: 1.2 ;
+
+}
+
+h3 {
+    color: #666633;
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+    font-size: 20px;
+    border-style: none;
+Le musée des Frères Wright
+
+}
+
+img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 8px;
+    border: dotted;
+}
+
+.navbar {
+    overflow: hidden;
+    background-color: #666633;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+.navbar a {
+    float: left;
+    font-size: 16px;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+.dropdown {
+    float: left;
+    overflow: hidden;
+}
+
+.dropdown .dropbtn {
+    cursor: pointer;
+    font-size: 16px;
+    border:none;
+    outline: none;
+    color: white;
+    padding: 14px 16px;
+    background-color: inherit;
+    font-family: inherit;
+    margin: 0;
+}
+
+.navbar a:hover, .dropdown:hover .dropbtn, .dropbtn:focus {
+    background-color:  #271a0c;
+}
+
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f9f9f9;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+
+}
+
+.dropdown-content a {
+    float: none;
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+    text-align: left;
+
+}
+
+.dropdown-content a:hover {
+    background-color: #ddd;
+}
+
+.show {
+    display: block;
+}
+
+div.transbox {
+    margin: 40px;
+    background-color: #000000;
+    border: none;
+    opacity: 0.5;
+    filter: alpha(opacity=60);
+    outline: 15px ;
+    outline-color: #ffffff ;
+    outline-offset: 15px;
+    padding: 30px;
+
+}
+
+div.transbox {
+    color: #ffffff;
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: justify;
+    font-size: 18px;
+    border: 15px ;
+    border-style: none;
+    outline-offset: 25px;
+    margin: 100%;
+    font-weight: none;
+}
+
+.inner-border {
+    background: #000;
+    color: #fff;
+    margin: 50px;
+    padding: 20px;
+    position: relative;
+    opacity: 0.7;
+    filter: alpha(opacity=60);
+}
+.inner-border:before {
+    border: 5px solid #000;
+    content: "";
+    position: absolute;
+    top: -10px;
+    bottom: -10px;
+    left: -10px;
+    right: -10px;
+}
+
+span {
+    float: left;
+    width: 0.7em;
+    font-size: 400%;
+    font-family: algerian, courier;
+    line-height: 80%;
+}
+*/
+
+.inner-border {
+    background: #000;
+    color: #fff;
+    margin: 50px;
+    padding: 20px;
+    position: relative;
+    opacity: 0.7;
+    filter: alpha(opacity=60);
+}
+.inner-border:before {
+    border: 5px solid #000;
+    content: "";
+    position: absolute;
+    top: -10px;
+    bottom: -10px;
+    left: -10px;
+    right: -10px;
+}
+
+span {
+    float: left;
+    width: 0.7em;
+    font-size: 400%;
+    font-family: algerian, courier;
+    line-height: 80%;
+}
+
+div.transbox {
+    color: #ffffff;
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: justify;
+    font-size: 18px;
+    border: 15px ;
+    border-style: none;
+    outline-offset: 25px;
+    margin: 100%;
+    font-weight: none;
+}
+
+.inner-border {
+    background: #000;
+    color: #fff;
+    margin: 50px;
+    padding: 20px;
+    position: relative;
+    opacity: 0.7;
+    filter: alpha(opacity=60);
+}
+.inner-border:before {
+    border: 5px solid #000;
+    content: "";
+    position: absolute;
+    top: -10px;
+    bottom: -10px;
+    left: -10px;
+    right: -10px;
+}
+*/
+span {
+    float: left;
+    width: 0.7em;
+    font-size: 400%;
+    font-family: algerian, courier;
+    line-height: 80%;
+}
+
+.navbar a:hover, .dropdown:hover .dropbtn, .dropbtn:focus {
+    background-color:  #271a0c;
+}
+
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f9f9f9;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+
+}
+
+.dropdown-content a {
+    float: none;
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+    text-align: left;
+
+}
+
+.dropdown-content a:hover {
+    background-color: #ddd;
+}
+
+.show {
+    display: block;
+}
+
+div.transbox {
+    margin: 40px;
+    background-color: #000000;
+    border: none;
+    opacity: 0.5;
+    filter: alpha(opacity=60); /* For IE8 and earlier */
+    outline: 15px ;
+    outline-color: #ffffff ;
+    outline-offset: 15px;
+    padding: 30px;
+
+}
+
+div.transbox {
+    color: #ffffff;
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: justify;
+    font-size: 18px;
+    border: 15px ;
+    border-style: none;
+    outline-offset: 25px;
+    margin: 100%;
+    font-weight: none;
+}
+
+.inner-border {
+    background: #000;
+    color: #fff;
+    margin: 50px;
+    padding: 20px;
+    position: relative;
+    opacity: 0.7;
+    filter: alpha(opacity=60); /* For IE8 and earlier */
+}
+.inner-border:before {
+    border: 5px solid #000;
+    content: "";
+    position: absolute;
+    top: -10px;
+    bottom: -10px;
+    left: -10px;
+    right: -10px;
+}
+
+span {
+    float: left;
+    width: 0.7em;
+    font-size: 400%;
+    font-family: algerian, courier;
+    line-height: 80%;
+}
+
 </style>
